0dbc61a9270c60348000e680ba5bd358af16f31f
johndoesstuff <johndoesstuff03@gmail.com>
2025-09-18 14:17:10 -0400

    faster

--- pSLOC.sh
+++ pSLOC.sh
@@ -8,23 +8,26 @@ if [[ -e "$output" ]]; then
     exit 1
 fi
 
+# search for all non-binary and non hidden files
 files=$(find . \
     -type f \
     ! -name "$(basename "$output")" \
     ! -path "./$output" \
     ! -path "*/.*" \
     ! -name '*Zone.Identifier*' \
-    -exec grep -Iq . {} \; \
-    -print | sort -u)
+    -exec sh -c 'head -c 4096 "$1" | grep -Iq . && echo "$1"' _ {} \; \
+    | sort -u)
 
+# init output
 > "$output"
 
-# search for all non-binary and non hidden files
+# cat files to output
 while IFS= read -r file; do
     echo "===== $file =====" >> "$output"
     cat "$file" >> "$output"
     echo -e "\n" >> "$output"
 done <<< "$files"
 
+# success
 lines=$(wc -l < "$output")
 printf "Written %d lines to %s\n" "$lines" "$output"
