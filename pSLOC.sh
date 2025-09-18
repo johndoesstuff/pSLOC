7d801150f0c8da9542d48426495e171cbe7c4e57
johndoesstuff <johndoesstuff03@gmail.com>
2025-09-18 13:51:36 -0400

    patches

--- pSLOC.sh
+++ pSLOC.sh
@@ -8,12 +8,16 @@ if [[ -e "$output" ]]; then
     exit 1
 fi
 
+files=$(find . -type f ! -name "$(basename "$output")" -exec file {} \; | grep text | cut -d: -f1 | awk '!/\/\./')
+
 > "$output"
 
 # search for all non-binary and non hidden files
-find . -type f ! -name "$(basename "$output")" -exec file {} \; | grep text | cut -d: -f1 | awk '!/\/\./' | while read -r file; do
+while IFS= read -r file; do
     echo "===== $file =====" >> "$output"
     cat "$file" >> "$output"
     echo -e "\n" >> "$output"
-done
+done <<< "$files"
 
+lines=$(wc -l < "$output")
+printf "Written %d lines to %s\n" "$lines" "$output"
